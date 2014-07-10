8df40a649a6af8edcf62b8819bfdc66e0d72585b
kougo <kougo@techmatrix.co.jp>
2014-07-10 13:19:08 +0900

    サンプル

--- ganga/src/main/java/com/karatebancho/ganga/GangaServlet.java
+++ ganga/src/main/java/com/karatebancho/ganga/GangaServlet.java
@@ -4,22 +4,37 @@
 package com.karatebancho.ganga;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStreamWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.markdown4j.Markdown4jProcessor;
+
 /**
  * @author ysobj
- *
  */
 public class GangaServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
 
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-		super.service(req, resp);
+		Path test = Paths.get("test.txt");
+		resp.setContentType("text/html; charset=utf-8");
+		try (InputStream is = Files.newInputStream(test);
+				OutputStreamWriter os = new OutputStreamWriter(
+						resp.getOutputStream())) {
+			os.write("<html><body>");
+			String content = new Markdown4jProcessor().process(is);
+			os.write(content);
+			os.write("</body></html>");
+		}
 	}
-	
 }
